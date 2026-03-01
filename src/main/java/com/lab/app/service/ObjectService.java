402265cf4640a9a0424faee1c330533608c484dd
koksiong <kksfum@gmail.com>
2026-03-01 22:14:46 +0800

    introduce cache for key value version

--- src/main/java/com/lab/app/service/ObjectService.java
+++ src/main/java/com/lab/app/service/ObjectService.java
@@ -1,13 +1,14 @@
 package com.lab.app.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.lab.app.dao.ObjectRepository;
-import com.lab.app.model.entity.KeyValue;
 import com.lab.app.model.dto.DataObject;
+import com.lab.app.model.entity.KeyValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ObjectService {
@@ -24,24 +27,29 @@ public class ObjectService {
     private final Logger logger = LoggerFactory.getLogger(ObjectService.class);
     private final ObjectRepository objectRepository;
 
+    private final Cache<String, AtomicLong> cache = Caffeine.newBuilder()
+            .maximumSize(10_000) // Evict if size exceeds 10,000
+            .expireAfterWrite(1, TimeUnit.MINUTES) // Expire 1 min after write
+            .recordStats() // Optional: enable statistics
+            .build();
+
     public ObjectService(ObjectRepository objectRepository) {
         this.objectRepository = objectRepository;
     }
 
     @Transactional
-    public List<DataObject> updateObjects(@RequestBody Map<String, String> inputs) {
+    public List<DataObject> updateObjects(Map<String, String> inputs) {
         final List<DataObject> list = new ArrayList<>();
 
         inputs.forEach((key, value) -> {
             logger.info("Key: {}, Value: {}", key, value);
-            Optional<KeyValue> dbObject = objectRepository.findTopByKvKeyOrderByCreatedDesc(key);
+            final AtomicLong version = cache.get(key, k -> {
+                Optional<KeyValue> dbObject = objectRepository.findTopByKvKeyOrderByCreatedDesc(key);
+                return dbObject.map(keyValue -> new AtomicLong(keyValue.getVersion()))
+                        .orElseGet(() -> new AtomicLong(0L));
+            });
             KeyValue kvEntity = new KeyValue();
-            if (dbObject.isPresent()) {
-                kvEntity.setVersion(dbObject.get().getVersion() + 1);
-            }
-            else {
-                kvEntity.setVersion(1L);
-            }
+            kvEntity.setVersion(version.incrementAndGet());
             kvEntity.setKvKey(key);
             kvEntity.setKvValue(value);
             kvEntity.setCreated(LocalDateTime.now(zoneId));
